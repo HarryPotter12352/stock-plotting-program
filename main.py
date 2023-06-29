2f249bf47057ad221c2426a766f01c716fc4b0ef
HarryPotter12352 <harrypottr12352.github@outlook.com>
2023-06-29 12:16:40 +0530

    Implemented formula and multiple stocks

--- main.py
+++ main.py
@@ -1,70 +1,52 @@
 import csv
 import matplotlib.pyplot as plt
 from yahoo_fin.stock_info import get_data
-import pandas as pd
 import os
 
-ticker1 = input("Enter ticker 1: ")
-ticker2 = input("Enter ticker 2: ")
+startDate = input("Enter start date in the format MM/DD/YYYY: ")
+endDate = input("Enter end date in the format MM/DD/YYYY: ")
+no = int(input("Enter number of stocks you want to compare: "))
+tickers = []
+if no < 2:
+    print("Enter a valid number")
+    exit()
+else:
+    for i in range(no):
+        tickers.append(input("Enter ticker: "))
+data = []
+for ticker in tickers:
+    initial = (get_data(ticker=ticker, start_date=startDate, end_date=endDate))
+    with open(f"{ticker}.csv", "w") as f:
+        f.write(initial.to_csv())
+    with open(f"{ticker}.csv", "r") as fin:
+        dataOfFile = fin.read().splitlines(True)
+    with open(f"{ticker}.csv", "w") as fout:
+        toWrite = dataOfFile[1:]
+        c = toWrite.count("\n")
+        for i in range(c):
+            toWrite.remove("\n")
+        fout.writelines(toWrite)
+    with open(f"{ticker}.csv", "r") as f:
+        plots = csv.reader(f, delimiter=",")
+        xList = []
+        yList = []
+        for row in plots:
+            xList.append((row[0]))
+            yList.append(int(round(float(row[1]))))
+        n = yList[0]
+        for i in range(len(yList)):
+            n1 = (yList[i] - n)/n*100
+            yList[i] = int(round(float(n1)))
+        data.append([xList,yList])
 
-data1 = get_data(ticker1, start_date="6/17/2023", end_date="6/27/2023")
-data2 = get_data(ticker2, start_date="6/17/2023", end_date="6/27/2023")
-with open(f"{ticker1}.csv", "w") as f:
-    f.write(data1.to_csv())
-with open(f"{ticker2}.csv", "w") as f:
-    f.write(data2.to_csv())
-df1 = pd.read_csv(f"{ticker1}.csv")
-df2 = pd.read_csv(f"{ticker2}.csv")
-df1.drop(index=df1.index[0], axis=0, inplace=True)
-df2 = df2[2:]
-df1.drop(df2.tail(1).index,inplace=True)
-df2.drop(df2.tail(1).index,inplace=True)
-x1 = []
-y1 = []
-x2 = []
-y2 = []
-with open(f'./{ticker1}.csv', 'r') as fin:
-    data = fin.read().splitlines(True)
-with open(f'./{ticker1}.csv', 'w') as fout:
-    toWrite = data[1:]
-    c = toWrite.count("\n")
-    for i in range(c):
-        toWrite.remove("\n")
-    fout.writelines(toWrite)
-with open(f'./{ticker2}.csv', 'r') as fin:
-    data = fin.read().splitlines(True)
-with open(f'./{ticker2}.csv', 'w') as fout:
-    toWrite = (data[1:])
-    c = toWrite.count("\n")
-    for i in range(c):
-        toWrite.remove("\n")
-    fout.writelines(toWrite)
+for i in range(no):
+    os.remove(f"{tickers[i]}.csv")
 
-with open(f"./{ticker1}.csv", 'r') as f:
-    plots = csv.reader(f, delimiter=",")
-    for row in plots:
-        x1.append((row[0]))
-        y1.append(int(round(float(row[1]))))
-    n = y1[0]
-    for i in range(len(y1)):
-        y1[i] = (y1[i] - n)/n*100
-
-with open(f"./{ticker2}.csv", "r") as f:
-    plots = csv.reader(f, delimiter=",")
-    for row in plots:
-        x2.append((row[0]))
-        y2.append(int(round(float(row[1]))))
-    n = y2[0]
-    for i in range(len(y2)):
-        y2[i] = (y2[i] - n)/n*100
-os.remove(f"{ticker1}.csv")
-os.remove(f"{ticker2}.csv")
-
-plt.plot(x1,y1, color = 'r', label = f"{ticker1}")
-plt.plot(x2,y2, color = 'b', label = f"{ticker2}")
+for i in range(len(data)):
+    plt.plot(data[i][0], data[i][1], label = tickers[i])
 plt.xlabel("Date")
+plt.title("Comparison graph")
 plt.ylabel("Price")
-plt.title(f"{ticker1}-{ticker2} comparison graph")
 plt.legend()
 plt.xticks(rotation = 45)
 plt.show()
